64bd19659001bc46e4a2e8fa6c213c47f7f1f9b0
Gabriel Zaffalon Ferreira Rocha <gabrielzaffalon19@gmail.com>
2024-03-11 21:37:35 -0300

    Adição dos codigos da aula 2

---
+++ Aula 2/main.py
@@ -0,0 +1,10 @@
+from Pessoa import Pessoa
+
+p1 = Pessoa("João", 52)
+print( "Nome da pessoa: ", p1.nome)
+
+p2 = Pessoa (idade = "25", nome = "João")
+print( "Nome da pessoa: ", p2.nome)
+
+p3 = Pessoa ( "Carlos" )
+print( "Nome da pessoa: ", p3.nome, "Idade: ", p3.idade)
